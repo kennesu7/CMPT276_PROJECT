12b356f633a4963def0d68f75d84626875243beb
tonyliang1102 <tonyliang1102@outlook.com>
2024-03-24 23:40:58 -0700

    update

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,25 +1,15 @@
 package com.example.demo.controllers;
-
 import java.security.Principal;
-
-import org.h2.engine.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.example.demo.dto.UserDto;
 import com.example.demo.service.UserService;
-
-import org.springframework.security.core.Authentication;
 
 
 
